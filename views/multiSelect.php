d9186b2ac2227b29188a3dfe6f8480676474e828
Meni Nuriel <nurielmeni@gmail.com>
2021-08-24 18:22:49 +0300

    v1.0.2

--- views/multiSelect.php
+++ views/multiSelect.php
@@ -1,3 +1,14 @@
+<?php
+
+use yii\bootstrap4\Dropdown;
+?>
+
 <div class="m-multiselect-wrapper">
   <h2><?= $text ?></h2>
+  <?= Dropdown::widget([
+    'items' => [
+      ['label' => 'DropdownA', 'url' => '/'],
+      ['label' => 'DropdownB', 'url' => '#'],
+    ],
+  ]) ?>
 </div>
