45da11d84036afc56bb831c147a2b14d963cffaf
Meni Nuriel <nurielmeni@gmail.com>
2021-08-24 18:34:51 +0300

    x

--- views/multiSelect.php
+++ views/multiSelect.php
@@ -5,10 +5,24 @@ use yii\bootstrap4\Dropdown;
 
 <div class="m-multiselect-wrapper">
   <h2><?= $text ?></h2>
-  <?= Dropdown::widget([
-    'items' => [
-      ['label' => 'DropdownA', 'url' => '/'],
-      ['label' => 'DropdownB', 'url' => '#'],
-    ],
-  ]) ?>
+  <p>
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      Button with data-target
+    </button>
+  </p>
+  <div class="collapse" id="collapseExample">
+    <div class="card card-body">
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+    </div>
+  </div>
+  <p>
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      Button with data-target
+    </button>
+  </p>
+  <div class="collapse" id="collapseExample">
+    <div class="card card-body">
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+    </div>
+  </div>
 </div>
