5b9b4d17c36b7ccfa24c46d668bf1963b427b65e
Meni Nuriel <nurielmeni@gmail.com>
2021-08-24 20:42:32 +0300

    s

--- views/multiSelect.php
+++ views/multiSelect.php
@@ -16,11 +16,11 @@ use yii\bootstrap4\Dropdown;
     </div>
   </div>
   <p>
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
       Button with data-target
     </button>
   </p>
-  <div class="collapse" id="collapseExample">
+  <div class="collapse" id="collapseExample1">
     <div class="card card-body">
       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
     </div>
