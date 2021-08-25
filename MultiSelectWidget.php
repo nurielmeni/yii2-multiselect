49dc7f00601e58abb8a957b0754d6b528ab9b489
Meni Nuriel <nurielmeni@gmail.com>
2021-08-25 12:47:20 +0300

    v1.0.6

--- MultiSelectWidget.php
+++ MultiSelectWidget.php
@@ -9,16 +9,24 @@ use nurielmeni\multiselect\assets\MultiSelectAsset;
  */
 class MultiSelectWidget extends \yii\base\Widget
 {
+    public $name = '';
+    public $rtl = true;
+    public $label = '';
+
     public function init()
     {
         parent::init();
         MultiSelectAsset::register(\Yii::$app->view);
+
+        $this->name = empty($this->name) ? 'm-ms-' . rand(0, 100) : $this->name;
     }
 
     public function run()
     {
         return $this->render('multiSelect', [
             'text' => "Hellow from multi select",
+            'name' => $this->name,
+            'rtl' => $this->dir,
         ]);
     }
 }
--- assets/js/multiSelect.js
+++ assets/js/multiSelect.js
@@ -1,3 +1,20 @@
-$(".m-multiselect-wrapper h2").on("click", function () {
-  $(this).css("color", "blue");
-});
+(function($) {
+  var selectElements = [];
+
+  /**
+   * @param {dom element} el the select wrapper
+   */
+  var renderSelect = function(el) {
+    
+  }
+
+  $(document).ready(function() {
+    // 1. Get all multiselect elements
+    selectElements = $('m-multiselect-wrapper select');
+
+    // 2. Hide all select elements
+
+    // 3. Render each multiselect element
+
+  });
+}) (jQuery);
