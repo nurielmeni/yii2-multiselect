666db169646a255caa5dc35d3a133b8bbf0274c1
Meni Nuriel <nurielmeni@gmail.com>
2021-08-25 18:51:43 +0300

    v1.0.25

--- assets/js/multiSelect.js
+++ assets/js/multiSelect.js
@@ -1,13 +1,13 @@
-(function($) {
+(function ($) {
   var selectValues = [];
 
   /**
    * @param {dom element} el the select wrapper
    */
-  var updateValue = function(el) {
+  var updateValue = function (el) {
     if (!el) return;
 
-    var value = $('#' + el + '-options li[aria-selected="true"]').map(function(i, o) {
+    var value = $('#' + el + '-options li[aria-selected="true"]').map(function (i, o) {
       return $(o).attr('value');
     });
     selectValues = value.toArray();
@@ -17,7 +17,7 @@
   /**
    * @param {dom element} el the select wrapper
    */
-  var selectAll = function(el) {
+  var selectAll = function (el) {
     if (!el) return;
     $('#' + el + '-options li').attr('aria-selected', 'true');
     updateValue(el);
@@ -26,42 +26,42 @@
   /**
    * @param {dom element} el the select wrapper
    */
-  var removeAll = function(el) {
+  var removeAll = function (el) {
     if (!el) return;
     $('#' + el + '-options li').attr('aria-selected', 'false');
     updateValue(el);
   }
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     // EVEN HANDLERS
     // Toggle selected options
-    $('.m-multiselect-options li[role="option"]').on('click', function() {
+    $('.m-multiselect-options li[role="option"]').on('click', function () {
       this.ariaSelected = this.ariaSelected === "true" ? "false" : "true";
       $(this).focus();
       updateValue($(this).parents('.m-multiselect-wrapper').data('el-id'));
     });
 
     // Select all listener
-    $('.m-multiselect-wrapper .card-footer.action button.select-all').on('click', function() {
+    $('.m-multiselect-wrapper .card-footer.action button.select-all').on('click', function () {
       selectAll($(this).parents('.m-multiselect-wrapper').data('el-id'));
     });
 
     // Remove all listener
-    $('.m-multiselect-wrapper .card-footer.action button.remove-all').on('click', function() {
+    $('.m-multiselect-wrapper .card-footer.action button.remove-all').on('click', function () {
       removeAll($(this).parents('.m-multiselect-wrapper').data('el-id'));
     });
 
     // Set focus to button
-    $('.m-multiselect-wrapper .collapse').on('shown.bs.collapse', function() {
+    $('.m-multiselect-wrapper .collapse').on('shown.bs.collapse', function () {
       $(this).find('li[role="option"]:first-child').focus();
     });
-    
+
     // Set focus to first option
-    $('.m-multiselect-wrapper .collapse').on('hidden.bs.collapse', function() {
+    $('.m-multiselect-wrapper .collapse').on('hidden.bs.collapse', function () {
       $(this).parents('.m-multiselect-wrapper').find('p.m-multiselect-button button:first-child').focus();
     });
 
-    $('.m-multiselect-wrapper').on('keydown', function(e) {
+    $('.m-multiselect-wrapper').on('keydown', function (e) {
       e = e || window.event;
 
       if (e.keyCode == '13') {
@@ -70,16 +70,16 @@
       }
     });
 
-    $('.m-multiselect-options li[role="option"]').on('keydown', function(e) {
+    $('.m-multiselect-options li[role="option"]').on('keydown', function (e) {
       e = e || window.event;
       if (e.keyCode == '37') {
         // right arrow
-        document.activeElement.previousSibling.focus();
+        document.activeElement.nextElementSibling && document.activeElement.nextElementSibling.focus();
       }
       else if (e.keyCode == '39') {
         // left arrow
-        document.activeElement.nextSibling.focus();
+        document.activeElement.previousElementSibling && document.activeElement.previousElementSibling.focus();
       }
     });
   });
-}) (jQuery);
+})(jQuery);
