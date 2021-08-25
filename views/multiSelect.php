b593eb64341dba333b72263e195024c756e88591
Meni Nuriel <nurielmeni@gmail.com>
2021-08-25 12:44:36 +0300

    v1.0.5

--- views/multiSelect.php
+++ views/multiSelect.php
@@ -3,26 +3,28 @@
 use yii\bootstrap4\Dropdown;
 ?>
 
-<div class="m-multiselect-wrapper">
-  <h2><?= $text ?></h2>
+<div class="m-multiselect-wrapper" dir="<?= $rtl ? 'rtl' : 'ltr' ?>">
+  <select id="<?= $name ?>" multiple name="<?= $name ?>[]">
+    <option value="1">first</option>
+    <option value="2">secound</option>
+    <option value="3">third</option>
+    <option value="4">forth</option>
+  </select>
   <p>
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-      Button with data-target
+    <label for="<?= $name ?>-options"><?= $label ?></label>
+    <button id="<?= $name ?>-button" class="btn btn-primary" type="button" data-toggle="collapse" data-target="#<?= $name ?>-collapse" aria-expanded="false" aria-controls="<?= $name ?>-collapse">
+      <span class="selection-hint"></span>
+      <span class="material-icons" role="aria-img">expand_more</span>
+
     </button>
   </p>
-  <div class="collapse" id="collapseExample">
+  <div id="<?= $name ?>-collapse" class="collapse">
     <div class="card card-body">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-    </div>
-  </div>
-  <p>
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
-      Button with data-target
-    </button>
-  </p>
-  <div class="collapse" id="collapseExample1">
-    <div class="card card-body">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+      <ul id="<?= $name ?>-options"  class="m-multiselect-options" role="listbox">
+        <li role="option" aria-selected="false" value="1">first</li>
+        <li role="option" aria-selected="false" value="2">secound</li>
+        <li role="option" aria-selected="false" value="3">third</li>
+      </ul>
     </div>
   </div>
 </div>
